404539cf15a57ac29ded3bd5e29ed9f1bc6c51c3
anish12341 <anishpatel345@gmail.com>
2020-11-16 12:01:20 -0600

    - Updated models for Users, Highlights - Created models for Collabs, Collab_user, Collab_space_highlights

---
+++ highlighter-backend/highlighter/server/models/collabUser.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const collabUser = sequelize.define('collab_user', {
+    space_id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true
+    },
+    user_id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true
+    },
+    isAdmin: DataTypes.BOOLEAN
+  }, {});
+  collabUser.associate = (models) => {
+    // associations can be defined here
+    collabUser.belongsTo(models.user, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE',
+    });
+
+    collabUser.belongsTo(models.collab, {
+      foreignKey: 'space_id',
+      onDelete: 'CASCADE',
+    });
+  };
+  collabUser.sync({alter: true}, ()=>{});
+  return collabUser;
+};
